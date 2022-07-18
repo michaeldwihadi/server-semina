dd1eb371b5c752ea5d9cc09123d0056f0935275a
Mich899 <michaeldwihadii@gmail.com>
2022-07-18 20:29:57 +0800

    create API Participants

--- app/config/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-module.exports = {
-  urlDb: process.env.URL_MONGODB_DEV,
-  jwtSecret: process.env.SECRET_KEY,
-  jwtExpiration: "24h", // 1 minute
-  jwtRefreshExpiration: 120, // 2 minutes
-};
